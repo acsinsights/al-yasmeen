a48e5ab24a0f69fb6b3be9c6326b4b2ba1fceb0e
Kishan-Yadav-Acetrot <98552345+Acetrot-Kishan@users.noreply.github.com>
2023-10-01 18:01:18 +0530

    edit project done

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -63,52 +63,70 @@ class ProjectController extends Controller
 
     public function edit($id)
     {
-        $projects = Project::findOrFail($id);
+        $project = Project::findOrFail($id);
 
-        return view('admin.project.edit', compact('projects'));
+        return view('admin.project.edit', compact('project'));
     }
     public function update(Request $request, $id)
     {
-        $project = Project::findOrFail($id);
-        if ($request->hasFile("cover")) {
-            if (file::exists("projects-img/" . $project->preview_img)) {
-                File::delete("projects-img/" . $project->preview_img);
-            }
-            $file = $request->file("cover");
-            $project->preview_img = time() . "_" . $file->getClientOriginalName();
-            $file->move(\public_path("/projects-img"), $project->preview_img);
-            $request['cover'] = $project->preview_img;
-        }
-
-        $project->update([
-            "projecttitle" => $request->projecttitle,
-            "subservice" => $request->subservice,
-            "preview_img" => $project->preview_img,
-            "date" =>  $request->date,
-            "hyperlinks" => $request->hyperlinks,
-
+        $request->validate([
+            "title" => "required|string|min:3|max:255",
+            "company_name" => "required|string",
+            "company_logo" => "nullable|image|mimes:jpeg,png,jpg|max:1024",
+            "description" => "required|string",
+            "preview_img" => "nullable|image|mimes:jpeg,png,jpg|max:1024",
+            "location" => "required|string",
+            "date" => "required|date",
         ]);
+        $project = Project::findOrFail($id);
+        if ($request->hasFile("company_logo")) {
+            $company_logo_file = $request->file("company_logo");
+            $company_logo_image =  ImageResizer::resizeImage($company_logo_file, 300);
+            $company_logo_name = Str::slug($request->title) . "_" . time() . "_" . $company_logo_file->getClientOriginalName();
+            $destinationPath = public_path('project/client/');
+            $company_logo_image->save($destinationPath . $company_logo_name);
+            // delete old image
+            if (File::exists("project/client/" . $project->company_logo)) {
+                File::delete("project/client/" . $project->company_logo);
+            }
+        }
+        if ($request->hasFile("preview_img")) {
+            $preview_img_file = $request->file("preview_img");
+            $preview_img_image =  ImageResizer::resizeImage($preview_img_file, 300);
+            $preview_img_name =  Str::slug($request->title) . "_" . time() . "_" . $preview_img_file->getClientOriginalName();
+            $destinationPath = public_path('project/preview/');
+            $preview_img_image->save($destinationPath . $preview_img_name);
+            // delete old image
+            if (File::exists("project/preview/" . $project->preview_img)) {
+                File::delete("project/preview/" . $project->preview_img);
+            }
+        }
+        $project->title = $request->title;
+        $project->company_name = $request->company_name;
+        if ($request->hasFile("company_logo")) {
+            $project->company_logo = $company_logo_name;
+        }
+        $project->description = $request->description;
+        if ($request->hasFile("preview_img")) {
+            $project->preview_img = $preview_img_name;
+        }
+        $project->location = $request->location;
+        $project->date = $request->date;
+        $project->save();
 
-        return redirect("/admin/project")->with('success', 'Post Updated Successfully');
+        return redirect()->route('admin.project.index')->with('success', 'Project updated successfully.');
     }
 
     public function destroy($id)
     {
         $project = Project::findOrFail($id);
-
-        if (File::exists("projects-img/" . $project->preview_img)) {
-            File::delete("projects-img/" . $project->preview_img);
+        if (File::exists("project/client/" . $project->company_logo)) {
+            File::delete("project/client/" . $project->company_logo);
+        }
+        if (File::exists("project/preview/" . $project->preview_img)) {
+            File::delete("project/preview/" . $project->preview_img);
         }
         $project->delete();
-        return back()->with('success', 'Post Deleted Successfully');
-    }
-
-    public function deletecover($id)
-    {
-        $cover = Project::findOrFail($id)->cover;
-        if (File::exists("cover/" . $cover)) {
-            File::delete("cover/" . $cover);
-        }
-        return back();
+        return back()->with('success', 'Project deleted successfully.');
     }
 }
