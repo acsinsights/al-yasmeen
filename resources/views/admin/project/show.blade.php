b016abaa0dc7648ee53d24040916bfe5c8665331
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-10-04 18:35:57 +0530

    just remove comments in show page

--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -7,9 +7,6 @@
         <div class="header-navbar-shadow"></div>
         <div class="content-wrapper">
             <div class="content-header row">
-
-
-
                 <div class="content-header-left col-md-9 col-12 mb-2">
                     <div class="row breadcrumbs-top">
                         <div class="col-12">
@@ -37,7 +34,6 @@
                 </div>
                 <section class="app-ecommerce-details">
                     <div class="card">
-                        <!-- Product Details starts -->
                         <div class="card-body">
                             <div class="row my-2">
                                 <div class="col-12 col-md-5 d-flex align-items-center justify-content-center mb-2 mb-md-0">
@@ -47,24 +43,15 @@
                                 </div>
                                 <div class="col-12 col-md-7">
                                     <h2 style="font-weight: 800"> {{ $project->title }}</h2>
-
-
                                     <p class="card-text">
                                         {{ $project->description }}
                                     </p>
-
-
                                 </div>
                             </div>
                         </div>
-                        <!-- Product Details ends -->
-
-
-                        <!-- Related Products starts -->
                         <div class="card-body">
                             <div class=" text-center">
                                 <h4>Other Images</h4>
-
                             </div>
                             <div class="col-12 pt-5">
                                 <div class="row">
@@ -96,159 +83,9 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- Related Products ends -->
                     </div>
                 </section>
-
-                {{-- <div class="content-detached content-left">
-                    <div class="">
-                        <div class="blog-detail-wrapper">
-                            <div class="row">
-                                <div class="p-5 ">
-                                    <div class="card">
-                                        <img style="height: 400px;width:400px;"
-                                            src="{{ url('project/preview/' . $project->preview_img) }}"
-                                            class="img-fluid card-img-top" alt="Blog Detail Pic">
-                                        <div class="card-body">
-                                            <h4 class="card-title"> {{ $project->title }}</h4>
-                                            <p class="card-text mb-2">
-                                                {{ $project->description }}
-                                            </p>
-
-                                            <div class="align-items-center media">
-                                                <div class=" mr-2">
-                                                    <h6>Location-{{ $project->location }} </h6>
-                                                    <h6>Date-{{ $project->location }}</h6>
-                                                </div>
-
-                                                <div class="avatar mr-2">
-                                                    <img src="{{ url('project/client/' . $project->company_logo) }}"
-                                                        width="60" height="60" alt="Avatar">
-                                                </div>
-
-                                            </div>
-                                            <hr class="my-2">
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-6 pt-5">
-                                    <div class="row">
-                                        @forelse ($images as $img)
-                                            <div class="col-lg-4 col-md-4 col-6">
-                                                <div class="card">
-                                                    <div class="card-content">
-                                                        <div class="card-body">
-                                                            <img class="card-img-top img-fluid"
-                                                                src="{{ url('project/image/' . $img->image) }}"
-                                                                alt="Card image cap">
-                                                        </div>
-                                                        <div class="card-footer">
-                                                            <a onclick="return confirm('Are you sure?')"
-                                                                class="btn btn-danger"
-                                                                href="{{ route('admin.project.image.destroy', [$project->id, $img->id]) }}">
-
-                                                                <i class="fa fa-trash"></i>
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        @empty
-                                            <div class="text-danger col-12">
-                                                No Image Found
-                                            </div>
-                                        @endforelse
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
             </div>
-            {{-- <div class="content-body">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row" id="basic-table">
-                            <div class="col-md-6 col-12 mb-2">
-                                <b>
-                                    Project Title:
-                                </b>
-                                {{ $project->title }}
-                            </div>
-                            <div class="col-md-6 col-12 mb-2">
-                                <b>
-                                    Company Name:
-                                </b>
-                                {{ $project->company_name }}
-                            </div>
-                            <div class="col-md-6 col-12 mb-2">
-                                <b>
-                                    Company Logo: <br>
-                                </b>
-                                <img src="{{ url('project/client/' . $project->company_logo) }}" class="img-responsive"
-                                    style="max-height:100px; max-width:100px" alt="">
-                            </div>
-                            <div class="col-md-6 col-12 mb-2">
-                                <b>
-                                    Preview Image: <br>
-                                </b>
-                                <img src="{{ url('project/preview/' . $project->preview_img) }}" class="img-responsive"
-                                    style="max-height:100px; max-width:100px" alt="">
-                            </div>
-                            <div class="col-md-6 col-12 mb-2">
-                                <b>
-                                    Location:
-                                </b>
-                                {{ $project->location }}
-                            </div>
-                            <div class="col-md-6 col-12 mb-2">
-                                <b>
-                                    Date:
-                                </b>
-                                {{ date('d-m-Y', strtotime($project->date)) }}
-                            </div>
-                            <div class="col-12 mb-2">
-                                <b>
-                                    Project Description:
-                                </b>
-                                <br>
-                                {{ $project->description }}
-                            </div>
-                            <div class="col-12 mb-2">
-                                <div class="row">
-
-                                    @forelse ($images as $img)
-                                        <div class="col-lg-3 col-md-4 col-6">
-                                            <div class="card">
-                                                <div class="card-content">
-                                                    <div class="card-body">
-                                                        <img class="card-img-top img-fluid"
-                                                            src="{{ url('project/image/' . $img->image) }}"
-                                                            alt="Card image cap">
-                                                    </div>
-                                                    <div class="card-footer">
-                                                        <a onclick="return confirm('Are you sure?')" class="btn btn-danger"
-                                                            href="{{ route('admin.project.image.destroy', [$project->id, $img->id]) }}">
-                                                            Delete
-                                                            <i class="fa fa-trash"></i>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @empty
-                                        <div class="text-danger col-12">
-                                            No Image Found
-                                        </div>
-                                    @endforelse
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div> --}}
         </div>
     </div>
 
