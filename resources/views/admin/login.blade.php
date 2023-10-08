4d865cbbfed00f589b22d5878ca3e7724a5229e1
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-10-08 09:45:05 +0530

    alert msg added in login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
@@ -7,36 +6,41 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description"
+        content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>AL Yasmeen - Login Page</title>
-    <link rel="apple-touch-icon" href="{{ asset('user-assets/img/logo/favicon.ico')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('user-assets\images\al-yasmeen.svg') }}" width="132" height="72" alt="logo">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+    <link rel="apple-touch-icon" href="{{ asset('user-assets/img/logo/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('user-assets\images\al-yasmeen.svg') }}" width="132"
+        height="72" alt="logo">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
+        rel="stylesheet">
 
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/vendors/css/vendors.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/vendors/css/vendors.min.css') }}">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/bootstrap.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/bootstrap-extended.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/colors.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/components.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/themes/dark-layout.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/themes/bordered-layout.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/themes/semi-dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/bootstrap-extended.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/colors.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/components.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/themes/dark-layout.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/themes/bordered-layout.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/themes/semi-dark-layout.css') }}">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/core/menu/menu-types/vertical-menu.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/plugins/forms/form-validation.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/pages/page-auth.css')}}">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin-assets/css/core/menu/menu-types/vertical-menu.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/plugins/forms/form-validation.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/pages/page-auth.css') }}">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/style.css') }}">
     <!-- END: Custom CSS-->
 
 </head>
@@ -44,7 +48,8 @@
 
 <!-- BEGIN: Body-->
 
-<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="blank-page">
+<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click"
+    data-menu="vertical-menu-modern" data-col="blank-page">
     <!-- BEGIN: Content-->
     <div class="app-content content ">
         <div class="content-overlay"></div>
@@ -54,26 +59,20 @@
             </div>
             <div class="content-body">
                 <div class="auth-wrapper auth-v1 px-2">
-
-
-
                     <div class="auth-inner py-2">
-
                         <!-- Login v1 -->
                         <div class="card mb-0">
                             <div class="card-body">
-                               <a class="navbar-brand" href="index.html">
+                                <a class="navbar-brand" href="index.html">
                                     {{-- <img src="{{ asset('user-assets/img/logo/adfj.svg') }}" class="header-brand-img desktop-lgo"
                                 alt="AdFj Studios" style="margin-right: 70px;"> --}}
                                     <a href="{{ '/' }}" aria-label="logo image">
-                                        <img style="width:190px;margin-left: 69px;" src="{{ asset('user-assets\images\al-yasmeen.svg') }}"
-                                            width="132" height="72" alt="logo" />
+                                        <img style="width:190px;margin-left: 69px;"
+                                            src="{{ asset('user-assets\images\al-yasmeen.svg') }}" width="132"
+                                            height="72" alt="logo" />
                                     </a>
-
                                     {{-- <h2 class="brand-text">ADFJ</h2> --}}
                                 </a>
-
-
                                 <h4 class="card-title mb-1">Welcome to Dashboard! 👋</h4>
                                 <p class="card-text mb-2">Please sign-in to your account and start the adventure</p>
 
@@ -81,7 +80,9 @@
                                     @csrf
                                     <div class="form-group">
                                         <label class="form-label">Email</label>
-                                        <input type="email" class="form-control" name="email" placeholder="email" autofocus="" required>
+                                        <input type="email" class="form-control" name="email" placeholder="email"
+                                            autofocus="" required>
+
                                     </div>
 
                                     <div class="form-group">
@@ -90,32 +91,34 @@
 
                                         </div>
                                         <div class="input-group input-group-merge form-password-toggle">
-                                            <input type="password" class="form-control form-control-merge" name="password" placeholder="Password" required>
+                                            <input type="password" class="form-control form-control-merge"
+                                                name="password" placeholder="Password" required>
                                             <div class="input-group-append">
-                                                <span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span>
+                                                <span class="input-group-text cursor-pointer"><i
+                                                        data-feather="eye"></i></span>
                                             </div>
                                         </div>
                                     </div>
-
-                                        <!-- Remember Me -->
-        {{-- <div class="block mt-4">
+                                    <x-input-error :messages="$errors->get('email')" class="alert alert-danger mt-1 alert-validation-msg err-msg mt-2 " />
+                                    <!-- Remember Me -->
+                                    {{-- <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div> --}}
 
-        <div class="flex items-center justify-end mb-2">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-        </div>
+                                    <div class="flex items-center justify-end mb-2">
+                                        @if (Route::has('password.request'))
+                                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                                href="{{ route('password.request') }}">
+                                                {{ __('Forgot your password?') }}
+                                            </a>
+                                        @endif
+                                    </div>
                                     @include('admin.message')
                                     <button class="btn btn-primary btn-block" tabindex="4">Sign in</button>
                                 </form>
-
                             </div>
                         </div>
                         <!-- /Login v1 -->
@@ -129,20 +132,20 @@
 
 
     <!-- BEGIN: Vendor JS-->
-    <script src="{{ asset('admin-assets/vendors/js/vendors.min.js')}}"></script>
+    <script src="{{ asset('admin-assets/vendors/js/vendors.min.js') }}"></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
-    <script src="{{ asset('admin-assets/vendors/js/forms/validation/jquery.validate.min.js')}}"></script>
+    <script src="{{ asset('admin-assets/vendors/js/forms/validation/jquery.validate.min.js') }}"></script>
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="{{ asset('admin-assets/js/core/app-menu.js')}}"></script>
-    <script src="{{ asset('admin-assets/js/core/app.js')}}"></script>
+    <script src="{{ asset('admin-assets/js/core/app-menu.js') }}"></script>
+    <script src="{{ asset('admin-assets/js/core/app.js') }}"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
-    <script src="{{ asset('admin-assets/js/scripts/pages/page-auth-login.js')}}"></script>
+    <script src="{{ asset('admin-assets/js/scripts/pages/page-auth-login.js') }}"></script>
     <!-- END: Page JS-->
 
     <script>
@@ -157,4 +160,5 @@
     </script>
 </body>
 <!-- END: Body-->
+
 </html>
