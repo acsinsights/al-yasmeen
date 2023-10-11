4874c5158393cf1ccc1bb8e4c6209e04f8faf767
Kishan-Yadav-Acetrot <98552345+Acetrot-Kishan@users.noreply.github.com>
2023-10-11 11:39:34 +0530

    add image popup

--- resources/views/admin/includes/app.blade.php
+++ resources/views/admin/includes/app.blade.php
@@ -6,19 +6,24 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description"
+        content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title>AL Yasmeen</title>
     <link rel="apple-touch-icon" href="{{ asset('admin-assetsimages/ico/apple-icon-120.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('user-assets\images\al-yasmeen.svg') }}" width="132" height="72" alt="logo">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('user-assets\images\al-yasmeen.svg') }}" width="132"
+        height="72" alt="logo">
 
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
+        rel="stylesheet">
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
     <!-- BEGIN: Vendor CSS-->
     <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/vendors/css/vendors.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/vendors/css/charts/apexcharts.css') }}">
@@ -35,10 +40,12 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/themes/semi-dark-layout.css') }}">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/core/menu/menu-types/vertical-menu.css') }}">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin-assets/css/core/menu/menu-types/vertical-menu.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/pages/dashboard-ecommerce.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/plugins/charts/chart-apex.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/css/plugins/extensions/ext-component-toastr.css') }}">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin-assets/css/plugins/extensions/ext-component-toastr.css') }}">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
@@ -50,33 +57,34 @@
 
 <!-- BEGIN: Body-->
 
-<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static" data-open="click" data-menu="vertical-menu-modern" data-col="">
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static" data-open="click"
+    data-menu="vertical-menu-modern" data-col="">
 
-            @include('admin.includes.sidebar')
-            <!-- App-Content -->
-            <div class="app-content main-content">
-                <div class="side-app">
+    @include('admin.includes.sidebar')
+    <!-- App-Content -->
+    <div class="app-content main-content">
+        <div class="side-app">
 
-                    @include('admin.includes.header')
-                    @yield('content')
-                </div>
-            </div>
-            <!-- End app-content-->
-
-
-
+            @include('admin.includes.header')
+            @yield('content')
+        </div>
+    </div>
+    <!-- End app-content-->
 
 
 
 
 
-            <div class="sidenav-overlay"></div>
-            <div class="drag-target"></div>
 
-            <!-- BEGIN: Footer-->
 
-            <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
-            <!-- END: Footer-->
+
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+    <!-- BEGIN: Footer-->
+
+    <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
+    <!-- END: Footer-->
 
 
 
@@ -88,28 +96,28 @@
 
 
     <!-- BEGIN: Vendor JS-->
-    <script src="{{ asset('admin-assets/vendors/js/vendors.min.js')}}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/vendors.min.js') }}""></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
-    <script src="{{ asset('admin-assets/vendors/js/charts/apexcharts.min.js')}}""></script>
-    <script src="{{ asset('admin-assets/vendors/js/extensions/toastr.min.js')}}""></script>
-    <script src="{{ asset('admin-assets/vendors/js/extensions/moment.min.js')}}""></script>
-    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/datatables.min.js')}}""></script>
-    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/datatables.buttons.min.js')}}""></script>
-    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js')}}""></script>
-    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/dataTables.responsive.min.js')}}""></script>
-    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/responsive.bootstrap.min.js')}}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/charts/apexcharts.min.js') }}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/extensions/toastr.min.js') }}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/extensions/moment.min.js') }}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/datatables.min.js') }}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/datatables.buttons.min.js') }}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js') }}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/dataTables.responsive.min.js') }}""></script>
+    <script src="{{ asset('admin-assets/vendors/js/tables/datatable/responsive.bootstrap.min.js') }}""></script>
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="{{ asset('admin-assets/js/core/app-menu.js')}}""></script>
-    <script src="{{ asset('admin-assets/js/core/app.js')}}""></script>
+    <script src="{{ asset('admin-assets/js/core/app-menu.js') }}""></script>
+    <script src="{{ asset('admin-assets/js/core/app.js') }}""></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
-    <script src="{{ asset('admin-assets/js/scripts/pages/dashboard-analytics.js')}}""></script>
-    <script src="{{ asset('admin-assets/js/scripts/pages/app-invoice-list.js')}}""></script>
+    <script src="{{ asset('admin-assets/js/scripts/pages/dashboard-analytics.js') }}""></script>
+    <script src="{{ asset('admin-assets/js/scripts/pages/app-invoice-list.js') }}""></script>
     <!-- END: Page JS-->
 
     <script>
@@ -122,31 +130,9 @@
             }
         })
     </script>
+    @yield('js')
 
+</body>
+<!-- END: Body-->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </body>
-        <!-- END: Body-->
-
-        </html>
+</html>
